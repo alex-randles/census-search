135c41f04d0e8c38b12f453d2d15683e9d550cab
alex <alexrandles0@gmail.com>
2024-09-04 11:04:40 +0100

    refactored code

--- census_dashboard.py
+++ census_dashboard.py
@@ -4,8 +4,12 @@ import plotly.graph_objects as go
 
 import streamlit as st  # pip install streamlit
 
+# Include custom favicon using HTML
+favicon_path = "https://vrti-graph.adaptcentre.ie/static/images/favicon.png"  # Replace with your favicon path or URL
+
+
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Census Dashboard", page_icon="static/images/favicon.png", layout="wide")
+st.set_page_config(page_title="Census Dashboard", page_icon=favicon_path, layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache_data
@@ -97,7 +101,7 @@ parish_selected = None
 #         st.stop()
 
 if number and townland_selected:
-# if number and (townland_selected and parish_selected):
+    # if number and (townland_selected and parish_selected):
     st.warning("Please use only one fiter at a time")
     st.stop()
 
@@ -300,105 +304,13 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown(f'    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">', unsafe_allow_html=True)
+hide_streamlit_style = """
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $(".viewerBadge_container__r5tak").remove();
+    });
+    </script>
+"""
 
-# st.markdown(open("templates/footer.html", "r+").read(), unsafe_allow_html=True)
-
-# ---- SIDEBAR ----
-# st.sidebar.header("Please Filter Here:")
-# city = st.sidebar.multiselect(
-#     "Select the City:",
-#     options=df["Townland"].unique(),
-#     default=df["Townland"].unique()
-# )
-#
-# customer_type = st.sidebar.multiselect(
-#     "Select the Customer Type:",
-#     options=df["Customer_type"].unique(),
-#     default=df["Customer_type"].unique(),
-# )
-#
-# gender = st.sidebar.multiselect(
-#     "Select the Gender:",
-#     options=df["Gender"].unique(),
-#     default=df["Gender"].unique()
-# )
-#
-# df_selection = df.query(
-#     "City == @city & Customer_type ==@customer_type & Gender == @gender"
-# )
-#
-# # Check if the dataframe is empty:
-# if df_selection.empty:
-#     st.warning("No data available based on the current filter settings!")
-#     st.stop() # This will halt the app from further execution.
-#
-# # ---- MAINPAGE ----
-# st.title(":bar_chart: Sales Dashboard")
-# st.markdown("##")
-#
-# # TOP KPI's
-# total_sales = int(df_selection["Total"].sum())
-# average_rating = round(df_selection["Rating"].mean(), 1)
-# star_rating = ":star:" * int(round(average_rating, 0))
-# average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-#
-# left_column, middle_column, right_column = st.columns(3)
-# with left_column:
-#     st.subheader("Total Sales:")
-#     st.subheader(f"US $ {total_sales:,}")
-# with middle_column:
-#     st.subheader("Average Rating:")
-#     st.subheader(f"{average_rating} {star_rating}")
-# with right_column:
-#     st.subheader("Average Sales Per Transaction:")
-#     st.subheader(f"US $ {average_sale_by_transaction}")
-#
-# st.markdown("""---""")
-#
-# # SALES BY PRODUCT LINE [BAR CHART]
-# sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
-# fig_product_sales = px.bar(
-#     sales_by_product_line,
-#     x="Total",
-#     y=sales_by_product_line.index,
-#     orientation="h",
-#     title="<b>Sales by Product Line</b>",
-#     color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-#     template="plotly_white",
-# )
-# fig_product_sales.update_layout(
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     xaxis=(dict(showgrid=False))
-# )
-#
-# # SALES BY HOUR [BAR CHART]
-# sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
-# fig_hourly_sales = px.bar(
-#     sales_by_hour,
-#     x=sales_by_hour.index,
-#     y="Total",
-#     title="<b>Sales by hour</b>",
-#     color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-#     template="plotly_white",
-# )
-# fig_hourly_sales.update_layout(
-#     xaxis=dict(tickmode="linear"),
-#     plot_bgcolor="rgba(0,0,0,0)",
-#     yaxis=(dict(showgrid=False)),
-# )
-#
-#
-# left_column, right_column = st.columns(2)
-# left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-# right_column.plotly_chart(fig_product_sales, use_container_width=True)
-#
-#
-# # ---- HIDE STREAMLIT STYLE ----
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
